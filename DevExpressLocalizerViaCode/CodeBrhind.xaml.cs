4426ab685a65fd74bd7beab937cec8bfb36667b9
Andrew <ondrya@yandex.ru>
2020-09-14 18:38:48 +0300

    Create CodeBrhind.xaml.cs

---
+++ DevExpressLocalizerViaCode/CodeBrhind.xaml.cs
@@ -0,0 +1,8 @@
+// add static method
+#region LOCALIZATION
+static UnscannedCargoWindow()
+{
+  GridControlLocalizer.Active = new CustomDXGridLocalizer();
+  EditorLocalizer.Active = new CustomFilterEditorLocalizer();
+}
+#endregion
